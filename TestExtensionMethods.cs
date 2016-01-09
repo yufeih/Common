4545ca04940a3b1ccb20864cbf1c5bcf0fcd3ad2
Yufei Huang <yufeih@live.com>
2016-01-09 20:12:56 +0800

    Add some additional helpers

---
+++ TestExtensionMethods.cs
@@ -0,0 +1,81 @@
+﻿namespace System
+{
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Nine.Formatting;
+
+    [DebuggerStepThrough]
+    static class TestExtensionMethods
+    {
+        private static readonly IFormatter json = new JsonFormatter();
+
+        public async static Task PropertyChangedTo<T>(this T obj, Func<T, bool> predicate, int timeout = 10000)
+        {
+            if (predicate(obj)) return;
+
+            var n = obj as INotifyPropertyChanged;
+            if (n == null) throw new TimeoutException();
+
+            var tcs = new TaskCompletionSource<int>();
+            var handler = (PropertyChangedEventHandler)null;
+            handler = new PropertyChangedEventHandler((sender, e) =>
+            {
+                if (predicate(obj))
+                {
+                    n.PropertyChanged -= handler;
+                    tcs.TrySetResult(0);
+                }
+            });
+            n.PropertyChanged += handler;
+
+            var delay = Task.Delay(timeout);
+            if (delay == await Task.WhenAny(delay, tcs.Task))
+            {
+                n.PropertyChanged -= handler;
+                throw new TimeoutException($"{ json.ToText(obj) }");
+            }
+        }
+
+        public static Task CollectionChangedTo<T>(this IEnumerable<T> collection, params Func<T, bool>[] elementPredicate)
+        {
+            var predicate = new Func<T[], bool>(array =>
+            {
+                if (array.Length != elementPredicate.Length) return false;
+                for (int i = 0; i < array.Length; i++) if (!elementPredicate[i](array[i])) return false;
+                return true;
+            });
+            return CollectionChangedTo(collection, predicate);
+        }
+
+        public async static Task CollectionChangedTo<T>(this IEnumerable<T> collection, Func<T[], bool> predicate, int timeout = 10000)
+        {
+            if (predicate(collection.ToArray())) return;
+
+            var collectionChanged = collection as INotifyCollectionChanged;
+            if (collectionChanged == null) throw new TimeoutException();
+
+            var tcs = new TaskCompletionSource<int>();
+            var handler = (NotifyCollectionChangedEventHandler)null;
+            handler = new NotifyCollectionChangedEventHandler((sender, e) =>
+            {
+                if (predicate(collection.ToArray()))
+                {
+                    collectionChanged.CollectionChanged -= handler;
+                    tcs.TrySetResult(0);
+                }
+            });
+            collectionChanged.CollectionChanged += handler;
+
+            var delay = Task.Delay(timeout);
+            if (delay == await Task.WhenAny(delay, tcs.Task))
+            {
+                collectionChanged.CollectionChanged -= handler;
+                throw new TimeoutException($"[{ collection.Count() }] : { json.ToText(collection) }");
+            }
+        }
+    }
+}
---
+++ UISynchronizationContext.cs
@@ -0,0 +1,96 @@
+namespace System.Threading
+{
+    using System.Collections.Concurrent;
+    using System.Diagnostics;
+
+    [DebuggerStepThrough]
+    class UISynchronizationContext : SynchronizationContext, IDisposable
+    {
+        struct WorkItem
+        {
+            public SendOrPostCallback callback;
+            public object state;
+            public AutoResetEvent handle;
+        }
+
+        public static UISynchronizationContext BindToCurrent()
+        {
+            var result = new UISynchronizationContext();
+            SetSynchronizationContext(result);
+            return result;
+        }
+
+        private bool disposed;
+        private Thread uiThread;
+        private SynchronizationContext previous = SynchronizationContext.Current;
+        private readonly BlockingCollection<WorkItem> workItems = new BlockingCollection<WorkItem>();
+        
+        private void EnsureUIThread()
+        {
+            if (uiThread == null)
+            {
+                lock (workItems)
+                {
+                    if (uiThread == null)
+                    {
+                        uiThread = new Thread(Loop) { Name = "UIThread" };
+                        uiThread.Start();
+                    }
+                }
+            }
+        }
+
+        private void Loop(object obj)
+        {
+            SetSynchronizationContext(this);
+
+            while (!disposed)
+            {
+                var currentItem = workItems.Take();
+
+                try
+                {
+                    currentItem.callback(currentItem.state);
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e);
+                }
+                finally
+                {
+                    if (currentItem.handle != null)
+                    {
+                        currentItem.handle.Set();
+                    }
+                }
+            }
+        }
+
+        public override void Post(SendOrPostCallback d, object state)
+        {
+            EnsureUIThread();
+            workItems.Add(new WorkItem { callback = d, state = state });
+        }
+
+        public override void Send(SendOrPostCallback d, object state)
+        {
+            if (Thread.CurrentThread == uiThread)
+            {
+                d(state);
+            }
+            else
+            {
+                EnsureUIThread();
+                var workItem = new WorkItem { callback = d, state = state, handle = new AutoResetEvent(false) };
+                workItems.Add(workItem);
+                workItem.handle.WaitOne();
+            }
+        }
+
+        public void Dispose()
+        {
+            disposed = true;
+            SynchronizationContext.SetSynchronizationContext(previous);
+        }
+    }
+}

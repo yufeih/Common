7e9c152c85b8a9e29d7e2cd343f9271336b7e0c6
Yufei Huang <yufeih@live.com>
2016-01-15 19:00:18 +0800

    Fix ObjectHelper<T>.Merge not working with inheritance

--- src/ObjectHelper.cs
+++ src/ObjectHelper.cs
@@ -1,52 +1,31 @@
 namespace System
 {
+    using System.Collections.Generic;
     using System.Reflection;
     using System.Linq;
-    using System.Collections.Generic;
 
-    static class ObjectHelper<T> where T : new()
+    static class ObjectHelper<T> where T : class, new()
     {
-        private static readonly PropertyInfo[] properties = (
-            from pi in typeof(T).GetTypeInfo().DeclaredProperties
-            where pi.GetMethod != null && pi.GetMethod.IsPublic && 
-                  pi.SetMethod != null && pi.SetMethod.IsPublic &&
-                  pi.GetIndexParameters().Length <= 0
+        private static readonly PropertyInfo[] mergeProperties = (
+            from pi in GetAllProperties(typeof(T))
+            where pi.GetMethod != null && pi.GetMethod.IsPublic && pi.SetMethod != null && pi.SetMethod.IsPublic
             select pi).ToArray();
 
-        private static readonly FieldInfo[] fields = (
-            from fi in typeof(T).GetTypeInfo().DeclaredFields where fi.IsPublic select fi).ToArray();
+        private static readonly FieldInfo[] mergeFields = (
+            from fi in GetAllFields(typeof(T)) where fi.IsPublic select fi).ToArray();
 
         public static T Merge(T target, T change)
         {
             if (Equals(target, change)) return target;
 
-            foreach (var prop in properties)
+            foreach (var pi in mergeProperties)
             {
-                prop.SetMethod.Invoke(target, new[] { prop.GetMethod.Invoke(change, null) });
+                pi.SetMethod.Invoke(target, new[] { pi.GetMethod.Invoke(change, null) });
             }
 
-            foreach (var field in fields)
+            foreach (var pi in mergeFields)
             {
-                field.SetValue(target, field.GetValue(change));
-            }
-
-            return target;
-        }
-
-        public static T MergeExclude(T target, T change, string[] excludedProperties)
-        {
-            if (Equals(target, change)) return target;
-
-            foreach (var prop in properties)
-            {
-                if (excludedProperties.Contains(prop.Name)) continue;
-                prop.SetMethod.Invoke(target, new[] { prop.GetMethod.Invoke(change, null) });
-            }
-
-            foreach (var field in fields)
-            {
-                if (excludedProperties.Contains(field.Name)) continue;
-                field.SetValue(target, field.GetValue(change));
+                pi.SetValue(target, pi.GetValue(change));
             }
 
             return target;
@@ -54,31 +33,34 @@ namespace System
 
         public static T Clone(T target)
         {
+            if (target == null) return null;
             return Merge(new T(), target);
         }
 
-        public static IEnumerable<string> Delta(T target, T comparand)
+        private static IEnumerable<PropertyInfo> GetAllProperties(Type type)
         {
-            if (Equals(target, comparand)) yield break;
-
-            foreach (var prop in properties)
+            while (type != null)
             {
-                if (!Equals(prop.GetMethod.Invoke(target, null), 
-                            prop.GetMethod.Invoke(comparand, null)))
+                var ti = type.GetTypeInfo();
+                foreach (var prop in ti.DeclaredProperties)
                 {
-                    yield return prop.Name;
+                    yield return prop;
                 }
+                type = ti.BaseType;
             }
+        }
 
-            foreach (var field in fields)
+        private static IEnumerable<FieldInfo> GetAllFields(Type type)
+        {
+            while (type != null)
             {
-                if (!Equals(field.GetValue(target),
-                            field.GetValue(target)))
+                var ti = type.GetTypeInfo();
+                foreach (var field in ti.DeclaredFields)
                 {
-                    yield return field.Name;
+                    yield return field;
                 }
+                type = ti.BaseType;
             }
-
         }
     }
 }

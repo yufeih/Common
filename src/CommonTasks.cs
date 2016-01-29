0c616c7d77c161daa9713f2a88b2c9e5e54e03b6
Yufei Huang <yufeih@live.com>
2016-01-29 16:38:02 +0800

    CommonTasks.Null<T>()

--- src/CommonTasks.cs
+++ src/CommonTasks.cs
@@ -9,5 +9,12 @@
         public static readonly Task<string> NullString = Task.FromResult<string>(null);
         public static readonly Task<string> EmptyString = Task.FromResult("");
         public static readonly Task<Stream> NullStream = Task.FromResult<Stream>(null);
+
+        public static Task<T> Null<T>() => Tasks<T>.Null;
+
+        class Tasks<T>
+        {
+            static readonly Task<T> Null = Task.FromResult<T>(null);
+        }
     }
 }

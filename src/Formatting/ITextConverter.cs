1c3f8974ffc856e524af927e0bdcdfecee2101a4
Yufei Huang <yufeih@live.com>
2016-06-23 19:58:39 +0800

    Remove Formatting

--- src/Formatting/ITextConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace System.Runtime.Serialization
-{
-    public interface ITextConverter { }
-    public interface ITextConverter<T> : ITextConverter
-    {
-        string ToText(T value);
-        T FromText(string text);
-    }
-}

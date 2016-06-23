2ac18e8885215adbf1ba39f9bab2d5b79ff070a9
Yufei Huang <yufeih@live.com>
2016-06-23 20:00:19 +0800

    Remove Formatter

--- src/Formatting/ITextConverter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nine.Formatting
-{
-    public interface ITextConverter { }
-    public interface ITextConverter<T> : ITextConverter
-    {
-        string ToText(T value);
-        T FromText(string text);
-    }
-}
